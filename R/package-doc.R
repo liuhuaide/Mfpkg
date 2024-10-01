dd247565ba79ce4c17eaad0fc8cdbd61565e8f1d
liuhuaide <jcakcino@gmail.com>
2024-10-01 14:29:06 +0200

    add Mgpkg package-doc and delete .DS_Store

---
+++ R/package-doc.R
@@ -0,0 +1,11 @@
+#' Mfpkg: A Package for Dijkstra and Euclidean Algorithms
+#'
+#' This package contains two functions: \code{Dijkstra} and \code{Euclidean}.
+#'
+#' @description This is an R package that provides implementations of Dijkstra's algorithm
+#' for shortest paths and the Euclidean algorithm for calculating the greatest common divisor.
+#'
+#' @name Mfpkg
+#' @aliases Mfpkg-package
+NULL
+
